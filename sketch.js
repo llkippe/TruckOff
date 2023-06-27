b8c1db2af23bb8a83a018d8bf143c970f737c0e5
llkippe <102752683+llkippe@users.noreply.github.com>
2023-06-27 15:19:01 +0200

    reposition score

--- sketch.js
+++ sketch.js
@@ -130,7 +130,7 @@ function draw() {
     fill(255)
     textAlign(CENTER);
     textSize(width/20);
-    text(score, 60, 60);
+    text(score, width/18, width/20);
 
     if(gamestate == "game ended") {
       drawGameEnded();
